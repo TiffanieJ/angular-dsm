acdff3a859fdb143e43f70ae05c3fd1b347f8aaa
Tjohnson <tmjohnson82@gmail.com>
2020-06-16 12:32:36 -0400

    adding knobs addon

--- src/stories/6-side-nav.stories.ts
+++ src/stories/6-side-nav.stories.ts
@@ -1,11 +1,13 @@
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { boolean, number, text, withKnobs } from '@storybook/addon-knobs';
 
 import { SideNavComponent } from '../app/components/side-nav/side-nav.component';
 
 export default {
   title: 'SideNav',
   component: SideNavComponent,
+  decorators: [withKnobs],
 };
 
 export const SideNav = () => ({
