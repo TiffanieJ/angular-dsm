eaa9481c7b8c4196f3a93ed87a39bef076b75eae
Tjohnson <tmjohnson82@gmail.com>
2020-07-09 14:00:03 -0400

    addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions/register', '@storybook/addon-links/register', '@storybook/addon-notes/register', '@storybook/addon-storysource'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-links/register',
+    '@storybook/addon-notes/register',
+    '@storybook/addon-storysource',
+    '@storybook/addon-docs/register',
+    '@storybook/addon-knobs/register'
+  ],
 };
